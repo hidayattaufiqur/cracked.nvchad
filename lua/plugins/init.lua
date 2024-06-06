067bf0e29120495c13d1e84fee4efaca2bb0c170
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-06-06 23:39:26 +0700

    Revert "Revert "fix: custom configs don't get imported""
    This reverts commit dc8e303b50c395d1ce4120e5ddc2aaf1d9680dab.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -79,12 +79,11 @@ local default_plugins = {
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
     build = ":TSUpdate",
     opts = function()
-      return require "plugins.configs.treesitter"
+      return require "custom.configs.treesitter"
     end,
     config = function(_, opts)
       dofile(vim.g.base46_cache .. "syntax")
       require("nvim-treesitter.configs").setup(opts)
-      require("custom.configs.treesitter")
     end,
   },
 
